09163908ed43b73984655ddd65958fec4cfd4b88
DialloAbdou <=dialloabdou@hotmail.com>
2023-11-15 01:22:06 +0100

    Amélioration du composant racin & ajout de mock

---
+++ ng-Books-app/src/app/book.ts
@@ -0,0 +1,12 @@
+
+export class Book {
+    id!: number;
+    Name!: string;
+    ShortDescription!: string;
+    LongDescription!: string
+    Price! : number;
+    ImageUrl!: string;
+    InStock ! : boolean;
+    CategoryId! : number;
+
+}
---
+++ ng-Books-app/src/app/mock-book-list.ts
@@ -0,0 +1,13 @@
+import { Book } from "./book"
+export const BOOKS: Book[] =[
+    {
+        id:1,
+        Name: "Nam1",
+        ShortDescription:"ShortDescription",
+        LongDescription : "LongDescription";
+        Price: 2,
+        ImageUrl :"ImageUrl",
+        InStock: false,
+        CategoryId : 1
+    }
+]
