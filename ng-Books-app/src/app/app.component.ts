f4003de26df20aded170e8428ebb9ffab424f71a
DialloAbdou <=dialloabdou@hotmail.com>
2023-11-17 01:20:26 +0100

    ajout d'un mock pour la liste des books

--- ng-Books-app/src/app/app.component.ts
+++ ng-Books-app/src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { Book } from './book';
+import { BOOKS } from './mock-book-list';
 
 @Component({
   selector: 'app-root',
   templateUrl:'./app.component.html'
 })
-export class AppComponent {
+export class AppComponent{
   title = 'Liste des Livres';
+  listLivres: Book[]|undefined
+  /*
+   * Les Constructeurs
+  */
+
+  /*
+   * Les Constructeurs
+  */
+  constructor()
+  {
+
+  }
 }
--- ng-Books-app/src/app/mock-book-list.ts
+++ ng-Books-app/src/app/mock-book-list.ts
@@ -2,12 +2,74 @@ import { Book } from "./book"
 export const BOOKS: Book[] =[
     {
         id:1,
-        Name: "Nam1",
-        ShortDescription:"ShortDescription",
-        LongDescription : "LongDescription";
-        Price: 2,
-        ImageUrl :"ImageUrl",
-        InStock: false,
+        Name: "Big Data For Dummies",
+        ShortDescription:"Big data management is one of the major challenges facing business, industry, and not-for-profit organizations",
+        LongDescription : "Big data management is one of the major challenges facing business, industry, and not-for-profit organizations,Data sets such as customer transactions for a mega-retailer, weather patterns monitored by meteorologists",
+        Price: 92,
+        ImageUrl :"https://images-na.ssl-images-amazon.com/images/I/51p6wBow%2B3L._SX389_BO1,204,203,200_.jpg",
+        InStock: true,
+        CategoryId : 1
+    },
+    {
+        id:2,
+        Name: "Big Data ",
+        ShortDescription:"Bernard Marr’s Data Strategy is a must-have guide to creating a robust data strategy",
+        LongDescription : "Bernard Marr’s Data Strategy is a must-have guide to creating a robust data strategy. Explaining how to identify your strategic data needs",
+        Price: 78,
+        ImageUrl :"https://images-na.ssl-images-amazon.com/images/I/41JjodHnKHL._SX331_BO1,204,203,200_.jpg",
+        InStock: true,
+        CategoryId : 1
+    },
+    {
+        id:3,
+        Name: "Database Engineering",
+        ShortDescription:"his book has been an evolving dream of ours for about five years",
+        LongDescription : "his book has been an evolving dream of ours for about five years. Laine came to the DBA role without any formal technical training",
+        Price: 52,
+        ImageUrl :"https://images-na.ssl-images-amazon.com/images/I/51UvR3a63OL._SX379_BO1,204,203,200_.jpg",
+        InStock: true,
+        CategoryId : 2
+    },
+    {
+        id:4,
+        Name: "Data-Intensiv",
+        ShortDescription:"Data is at the center of many challenges in system design today.",
+        LongDescription : "Data is at the center of many challenges in system design today. Difficult issues need to be figured out, such as scalability",
+        Price: 45,
+        ImageUrl :"https://images-na.ssl-images-amazon.com/images/I/51gP9mXEqWL._SX379_BO1,204,203,200_.jpg",
+        InStock: true,
+        CategoryId : 3
+
+    },
+
+    {
+        id:4,
+        Name: "Data-Intensiv",
+        ShortDescription:"Data is at the center of many challenges in system design today.",
+        LongDescription : "Data is at the center of many challenges in system design today. Difficult issues need to be figured out, such as scalability",
+        Price: 45,
+        ImageUrl :"https://images-na.ssl-images-amazon.com/images/I/51gP9mXEqWL._SX379_BO1,204,203,200_.jpg",
+        InStock: true,
+        CategoryId : 2
+    },
+    {
+        id:6,
+        Name: "Learning React",
+        ShortDescription:"Tf you want to learn how to build efficient user interfaces with React, this is your book",
+        LongDescription : "f you want to learn how to build efficient user interfaces with React, this is your book. Authors Alex Banks and Eve Porcello show you how to create UIs with this small JavaScript library that can deftly display data changes",
+        Price: 75,
+        ImageUrl :"https://images-eu.ssl-images-amazon.com/images/I/51Q43WRXJzL.jpg",
+        InStock: true,
+        CategoryId : 1
+    },
+    {
+        id:7,
+        Name: "Pro C# 7",
+        ShortDescription:"This essential classic title provides a comprehensive foundation in the C# programming language and the frameworks",
+        LongDescription : "This essential classic title provides a comprehensive foundation in the C# programming language and the frameworks it lives in. Now in its 8th edition, you’ll find all the very latest C# 7.1 and .NET 4.7 features here, along with four brand new chapters",
+        Price: 87,
+        ImageUrl :"https://images-na.ssl-images-amazon.com/images/I/413Z89zzezL._SX348_BO1,204,203,200_.jpg",
+        InStock: true,
         CategoryId : 1
     }
 ]
