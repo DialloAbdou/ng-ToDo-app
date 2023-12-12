bcffb03cf00dc866d6f6f786d8286e5a68a4c183
DialloAbdou <abdoudiallo3826@yahoo.fr>
2023-12-12 02:34:20 +0100

    ajout d'une fonction seclect

--- ng-Books-app/src/app/app.component.ts
+++ ng-Books-app/src/app/app.component.ts
@@ -9,14 +9,25 @@ import { BOOKS } from './mock-book-list';
 export class AppComponent implements OnInit {
   title = 'Liste des Livres';
   listLivres:Book[] = BOOKS
+  booke : Book |undefined
 
   ngOnInit(): void {
     console.log(this.listLivres) //affiche le tableau
   }
   
-  selectBook(event:MouseEvent){
-    const index:number= +(event.target as HTMLInputElement).value
-    console.log(` le nom du Livre est : ${this.listLivres[index].Name}`)
-  }
+   bookSelected(bookId : string){
+    this.booke = this.listLivres.find(b=>b.id==+bookId)
+    if(this.booke)
+    {
+      console.log(`le livre est ${this.booke.Name}`)
+    }else{
+      console.log("Ce livre n'existe pas")
+    }
+   }
+  
+  // selectBook(event:MouseEvent){
+  //   const index:number= +(event.target as HTMLInputElement).value
+  //   console.log(` le nom du Livre est : ${this.listLivres[index].Name}`)
+  // }
 
 }
